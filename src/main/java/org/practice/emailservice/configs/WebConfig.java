bd1491e7c64775ca3a285655b55d6bb7424b30b3
ransuum <129663862+ransuum@users.noreply.github.com>
2024-09-07 02:39:02 +0300

    d

--- src/main/java/org/practice/emailservice/configs/WebConfig.java
+++ src/main/java/org/practice/emailservice/configs/WebConfig.java
@@ -10,11 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .maxAge(3600L)
-                .allowedMethods("*")
+                .allowedOrigins("https://payable-harley-practika-62b11632.koyeb.app")  // Allow the specific origin (you can use "*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Upgrade")
                 .allowCredentials(true);
     }
 }
